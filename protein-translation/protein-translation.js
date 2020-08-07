121ad75a092bf2fcfe17b5c4dc354dce1ca0e871
riyaz <riyazpasha880@gmail.com>
2020-08-07 22:20:01 +0530

    protien trnaslation

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -2,8 +2,51 @@
 // This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const CODON_LENGTH = 3;
+const TerminatingCodons = ["UAA", "UAG", "UGA"];
+
+const codonToProtien = {
+  AUG: "Methionine",
+  UUU: "Phenylalanine",
+  UUC: "Phenylalanine",
+  UUA: "Leucine",
+  UUG: "Leucine",
+  UCU: "Serine",
+  UCC: "Serine",
+  UCA: "Serine",
+  UCG: "Serine",
+  UAU: "Tyrosine",
+  UAC: "Tyrosine",
+  UGU: "Cysteine",
+  UGC: "Cysteine",
+  UGG: "Tryptophan",
+}
+
+const isEmpty = (rnaSequence) => rnaSequence === null || rnaSequence === undefined;
+
+const isValid = (rnaSequence) => rnaSequence.length % CODON_LENGTH !== 0;
+
+const getRnaTranslation = (rnaSequence) => {
+  let protiens = [];
+  let codon = ""
+
+  rnaSequence.split("").forEach(char => {
+    codon += char;
+    if (codon.length === CODON_LENGTH) {
+      if (TerminatingCodons.includes(codon))
+        return;
+      if (!codonToProtien[codon])
+        throw Error("Invalid codon");
+      protiens.push(codonToProtien[codon]);
+      codon = "";
+    }
+  });
+
+  return protiens;
+}
 
 export const translate = (rnaSequence) => {
-  if (rnaSequence === "AUG") return ['Methionine']
-  return [];
+  if (isEmpty(rnaSequence)) return []
+  if (isValid(rnaSequence)) throw Error("Invalid codon")
+  return getRnaTranslation(rnaSequence);
 };
