59ccce2b34557b807ac85d6d0198227fa43e3005
riyaz <riyazpasha880@gmail.com>
2020-08-08 14:18:28 +0530

    roman numerals

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -3,6 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
+const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+const thousands = ["", "M", "MM", "MMM"]
+
 export const toRoman = (number) => {
-  return "I".repeat(number);
+  let roman = "";
+  roman += ones[number % 10];
+
+  number = parseInt(number / 10);
+  roman = tens[number % 10] + roman;
+
+  number = parseInt(number / 10);
+  roman = hundreds[number % 10] + roman;
+
+  number = parseInt(number / 10);
+  roman = thousands[number % 10] + roman;
+  return roman;
+
 };
