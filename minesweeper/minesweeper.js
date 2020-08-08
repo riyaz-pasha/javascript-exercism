1ee3a4e4e7321732141424ec179d561dda7c84dc
riyaz <riyazpasha880@gmail.com>
2020-08-08 22:15:56 +0530

    mine sweeper removed for loops

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -3,21 +3,14 @@
 // convenience to get you started writing code faster.
 //
 
-export const annotate = (input) => {
-  let annotedOutput = [];
-  for (let row = 0; row < input.length; row++) {
-    let annotedRow = ""
-    for (let column = 0; column < input[row].length; column++) {
-      if (input[row][column] === "*") annotedRow += "*"
-      else annotedRow += getMinesCount(input, row, column) || " "
-    }
-    annotedOutput[row] = annotedRow;
-  }
-  return annotedOutput
-}
+export const annotate = (input) => input
+  .map((row, rowIndex) => [...row]
+    .map((column, columnIndex) => column === "*" ? column : getMinesCount(input, rowIndex, columnIndex) || " ")
+    .join("")
+  )
 
 const getMinesCount = (input, row, column) =>
   (input[row].substring(column - 1, column + 2)
     + (input[row - 1] || " ").substring(column - 1, column + 2)
     + (input[row + 1] || " ").substring(column - 1, column + 2)
-  ).replace(/[^*]/g,"").length
+  ).replace(/[^*]/g, "").length
