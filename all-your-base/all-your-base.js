5f828d6a3f7937d2accd8989b8e05b1343e5ff97
riyaz <riyazpasha880@gmail.com>
2020-08-08 15:25:22 +0530

    all your base

---
+++ all-your-base/all-your-base.js
@@ -0,0 +1,35 @@
+//
+// This is only a SKELETON file for the 'All Your Base' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const isValid = (inputSeqArr, inputBase) => inputSeqArr === [0]
+  || (inputSeqArr.length > 0
+    && inputSeqArr[0] !== 0
+    && !inputSeqArr.every(number => number === 0)
+    && !inputSeqArr.some(number => number < 0)
+    && !inputSeqArr.some(number => number >= inputBase)
+  )
+
+const isValidBase = (base) => base >= 2 && parseInt(base) === base
+
+
+const getNumberInBase10 = (inputSeqArr, inputBase) => inputSeqArr
+  .reduce((sum, currentNumber, currentIndex, { length }) => sum += (currentNumber * inputBase ** (length - 1 - currentIndex)), 0);
+
+export const convert = (inputSeqArr, inputBase, outputBase) => {
+
+  if (!isValidBase(inputBase)) throw new Error('Wrong input base');
+  if (!isValidBase(outputBase)) throw new Error('Wrong output base');
+  if (!isValid(inputSeqArr, inputBase)) throw new Error('Input has wrong format');
+
+  let inputNumberInBase10 = getNumberInBase10(inputSeqArr, inputBase)
+
+  let numberInOutputBase = []
+  while (inputNumberInBase10 !== 0) {
+    numberInOutputBase = [inputNumberInBase10 % outputBase, ...numberInOutputBase]
+    inputNumberInBase10 = parseInt(inputNumberInBase10 / outputBase)
+  }
+
+  return numberInOutputBase
+};
