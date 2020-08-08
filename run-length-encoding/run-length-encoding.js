a3846519847859f8946a33b42fbddf41a599768c
riyaz <riyazpasha880@gmail.com>
2020-08-08 11:30:59 +0530

    encode string

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -3,24 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
-export const encode = (string) => {
-  let encodedString = ""
-  let prevChar
-  let count = 0
-  for (let char of string) {
-    if (prevChar === undefined) { prevChar = char; count = 1 }
-    else if (prevChar === char) count++;
-    else {
-      if (count === 1) encodedString += `${prevChar}`
-      else encodedString += `${count}${prevChar}`
-      count = 1
-      prevChar = char
-    }
-  }
-  if (count === 1) encodedString += `${prevChar}`
-  else if (prevChar) encodedString += `${count}${prevChar}`
-  return encodedString
-};
+export const encode = (string) => (string.match(/(.)\1*/g) || [])
+  .map(str => str.length === 1 ? str : `${str.length}${str[0]}`)
+  .reduce((encodedString, currentEncoding) => encodedString += currentEncoding, "")
 
 export const decode = () => {
   throw new Error("Remove this statement and implement this function");
